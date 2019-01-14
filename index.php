e150e37def6e8d7fa29185bda281e9f5cc641a97
Ivan Mikhaylenko <animasola@yandex.ru>
2019-01-14 07:08:20 +0300

    Something

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <header>
     </header>
     <main>
+        <div style="background: blue; color: white;text-align:center;font-size:30px;" onclick="modalWindow.showModalWindow('500','300','px','<div style=\'text-align: center; line-height: 300px;\'>Hello, World!!!</div>')">Кликни меня</div>
     </main>
     <footer>
     </footer>
